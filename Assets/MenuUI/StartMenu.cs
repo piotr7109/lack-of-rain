2e96636271bf95c21ca28d37f5a3857207b8f244
Piotr <piotr1_krk@interia.pl>
2018-01-02 20:01:55 +0100

    start menu and cleanup

---
+++ Assets/MenuUI/StartMenu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour {
+
+    public string startLevel;
+
+	public void StartNewGame() {
+        SceneManager.LoadScene(startLevel);
+    }
+}
